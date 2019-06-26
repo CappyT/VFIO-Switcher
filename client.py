14d91a020365d1547b4befe1dd5c6ba494c04959
Gabriele Turelli <cappy95@gmail.com>
2019-06-26 18:43:36 +0200

    Added try block in client

--- client.py
+++ client.py
@@ -4,9 +4,12 @@ import sys
 
 def main():
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.connect((sys.argv[1], int(sys.argv[2])))
-	s.sendall(b'run')
-	s.close()
+	try:
+		s.connect((sys.argv[1], int(sys.argv[2])))
+		s.sendall(b'run')
+		s.close()
+	except Exception as e:
+		print("Exception occourred while connecting: " + str(e))
 	sys.exit(0)
 
 
